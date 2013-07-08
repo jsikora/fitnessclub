b9ad269196873d0ad290bbc34f5ff7c8ddf1e7ba
jsikora <justyna.sudolska@gmail.com>
2013-07-08 11:21:49 +0200

    set empty user model , controller and view

--- module/User/module.php
+++ module/User/module.php
@@ -9,6 +9,11 @@
 
 namespace User;
 
+use User\Model\User;
+use User\Model\UserTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
 class Module
 {
     public function getAutoloaderConfig()
@@ -28,6 +33,25 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'User\Model\UserTable' =>  function($sm) {
+                    $tableGateway = $sm->get('UserTableGateway');
+                    $table = new UserTable($tableGateway);
+                    return $table;
+                },
+                'UserTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new User());
+                    return new TableGateway('user', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
     }
 
 }
